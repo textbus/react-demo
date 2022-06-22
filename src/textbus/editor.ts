243b8f7ef6b45a92f55572549296421bbca2405c
tanbo <464328895@qq.com>
2022-06-22 20:08:11 +0800

    feat: 更新 demo

--- src/textbus/editor.ts
+++ src/textbus/editor.ts
@@ -10,16 +10,16 @@ import { alertComponentLoader } from './components/alert/alert.component';
 import { alertTool } from './components/alert/alert.tool';
 import { AtPlugin } from './components/at/at.plugin';
 
-
 defaultComponentLoaders.push(alertComponentLoader)
 defaultComponentLoaders.unshift(alertComponentLoader)
 defaultTools.push([alertTool])
+
 export function createTextbusEditor(host: HTMLElement) {
   const editor = createEditor({
     plugins: [
-      new AtPlugin(),
-      new LinkJumpTipPlugin(),
-      new Toolbar(defaultTools)
+      () => new AtPlugin(),
+      () => new LinkJumpTipPlugin(),
+      () => new Toolbar(defaultTools)
     ]
   })
   editor.mount(host)
